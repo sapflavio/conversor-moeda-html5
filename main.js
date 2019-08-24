99abc3a2ffa37da4cd3f31d25a430239e6b6e47a
Flavio Mendes <sap.flavio@gmail.com>
2019-08-24 01:25:38 +0000

    remoção de funções globais

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+const ovly = { };
+
+ovly.conversor = { 
+	versao: "1",
+	chamarAPI: function(){
+		console.log("Chamando API");
+		const endpoint = "api/latest";
+		var oParametros = {
+			base: $("#moeda_origem").val()
+		};
+		$.get(endpoint, oParametros, this.callback );
+	},
+	callback: function(resultado){
+		// console.log(resultado);
+		var sMoedaDestino = $("#moeda_destino").val();
+		var fValor = resultado.rates[sMoedaDestino];
+		$("#valor_destino").text(fValor);
+		$("#moeda_destino2").text(sMoedaDestino);
+	}
+}
+
+/*
+
 function chamarAPI(){
 	console.log("Chamando API");
 	const endpoint = "api/latest";
@@ -15,4 +38,4 @@ function callback(resultado){
 	
 	$("#valor_destino").text(fValor);
 	$("#moeda_destino2").text(sMoedaDestino);
-}
+}*/
